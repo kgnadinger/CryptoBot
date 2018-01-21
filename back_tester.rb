3039e2d34ae7c7372351764c4a023ea71276a530
Kevin Gnadinger <kgnadinger@gmail.com>
2018-01-20 21:25:43 -0600

    commented up algorithm

--- back_tester.rb
+++ back_tester.rb
@@ -11,37 +11,75 @@ class BackTester
 	end
 
 	def go
+		# starting ven
 		ven_amount = 0
+
+		# starting eth
 		eth_amount = 0.01
+
+		# going to buy with 10% of the amount of eth left
 		eth_trading_chunks = eth_amount * @percentage_to_buy_with
+
+		# goign to sell 10% of my ven
 		ven_trading_chunks = ven_amount * @percentage_to_sell_with
+
+		# Grab all VenEth, ordered from smallest opening_time(integer)
 		venEthArray = VenEth.order(:opening_time).select(:id, :closing_price).all
+
 		venEthArray.each_with_index do |ven_eth, index|
+			# starting at 34 because that's how much price data I need to use the indicators
 			if index > 34
-				beginning_time = Time.now
+				puts "Index: #{index}"
+
+				# array of prices up to the index
 				price_history = venEthArray[0, index + 1].map { |eth| eth.closing_price }
+
+				# optomization, only going to calculate indicators with 500 points of price history
 				if price_history.count > 501
-					puts "chaning price history: #{index}"
 					price_history = price_history[index - 500, index]
 				end
+
+
 				data = Indicators::Data.new(price_history)
+
+				# calculate rsi
 				rsi = data.calc(:type => :rsi, :params => 14).output
+
+				# calculate MACD
 				macdArray = data.calc(:type => :macd, :params => [12, 26, 9]).output
+
+				# reference to rsi so I know whether to buy or sell
 				rsiAlert = rsi_recently_crossed_threshold?(rsi, index)
+
+				# test to see if rsi went to the buy/sell zones and a macd cross
 				if macd_recently_crossed?(macdArray, index) && rsiAlert[:crossed]
+
+					# rsi > 70
 					if rsiAlert[:buy]
+						# make sure I have enough eth to buy with
 						if eth_amount - eth_trading_chunks > 0
+
+							# "withdraw" the eth that I'm buying with
 							eth_amount = eth_amount - eth_trading_chunks
+
+							# amount of ven I'm gaining = amount of eth I'm buying with / price all times 0.0095 which is the amount with the fee taken out
 							new_ven = (eth_trading_chunks / ven_eth[:closing_price]) * (1 - @trading_fee)
 							ven_amount += new_ven
 							puts "New Ven Amount: #{ven_amount}, Price: #{ven_eth[:closing_price] * ven_amount}, Index: #{index}"
 						else
 							puts "Ran out of Eth"
 						end
+					# rsi < 30
 					elsif rsiAlert[:sell]
 						if ven_amount - ven_trading_chunks > 0
+
+							# "withdraw" the ven I'm selling
 							ven_amount = ven_amount - ven_trading_chunks
+
+							# price of ven I'm selling in terms of eth
 							sell_amount = ven_amount * ven_eth[:closing_price]
+
+							# take out the fee
 							new_eth = sell_amount * (1 - @trading_fee)
 							eth_amount += new_eth
 							puts "New Eth Amount: #{eth_amount}, Price: #{ven_eth[:closing_price]}, Index: #{index}"
@@ -50,8 +88,6 @@ class BackTester
 						end
 					end
 				end
-				puts "Time End: #{(Time.now - beginning_time) * 1000} milliseconds, Index: #{index}}"
-				beginning_time = Time.now
 			end
 		end
 		puts "Ven Amount = #{ven_amount}, Price in Eth = #{ven_amount * venEthArray.last[:closing_price]}"
@@ -59,9 +95,17 @@ class BackTester
 		
 	end
 
+	# the macd crosses when the historgram switches sign
 	def macd_recently_crossed?(macdArray, index)
+
+		# formatting the last and 2nd to last histogram height
 		firstMacd = format_number_to_be_larger_than_one(macdArray[macdArray.length - 2][0])
 		nextMacd = format_number_to_be_larger_than_one(macdArray[macdArray.length - 1][0])
+
+		# divide the 2 to see if there was a switch in sign. 
+		# If it went from negative to positive, then the result of the division would negative
+		# If it went from positive to negative, then the result of the division would also be negative
+		# If it stayed the same sign, then the result of the division would be positive
 		if ((firstMacd / nextMacd) < 0)
 			true
 		else
@@ -69,16 +113,19 @@ class BackTester
 		end
 	end
 
+	# format the number to make sure I dont run into floating point issues
 	def format_number_to_be_larger_than_one(number)
 		if (number < 1 && number > 0) || (number < 0 && number > -1)
 			number = number * 10
 		else
-			number
+			number.to_f
 		end
 	end
 
+	# just simply calculating whether or not the last 4 rsi numbers crossed into either threshold
+	# rsi will always cross before macd which is why I test a few places backwards
 	def rsi_recently_crossed_threshold?(rsiArray, index)
-		tolerance = 10
+		tolerance = 4
 		crossed = false
 		buy = false
 		sell = false
